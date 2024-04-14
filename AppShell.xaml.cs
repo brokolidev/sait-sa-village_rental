5cdf1da41d4d26fb278cb0743eb03d4f01af55a2
brokoli <bocalist@gmail.com>
2024-04-13 21:50:11 -0600

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VillageRentalsPrototype
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
